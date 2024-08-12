8074332a3f274e478a7f8af1a9cf7a7083051493
Monica-gs <monicagomesgomes47@gmail.com>
2024-08-12 19:42:50 -0300

    update url

--- public/url/base.js
+++ public/url/base.js
@@ -1,4 +1,4 @@
-const urlBaseAPI = 'http://localhost:3000';
-const urlBaseFront = 'http://127.0.0.1:5500/Monica_FrontEnd';
+const urlBaseAPI = 'https://escola-bdjj.onrender.com';
+const urlBaseFront = 'https://escola-bdjj.onrender.com';
 
 export { urlBaseAPI, urlBaseFront };
